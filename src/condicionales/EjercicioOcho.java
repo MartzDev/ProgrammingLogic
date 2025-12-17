35b86a152dea45a8808bf91511a40590f38d141c
MartzDev <155332741+MartzDev@users.noreply.github.com>
2025-12-17 18:24:33 -0500

    :hammer: logica del ejercicio ocho  actualizado

--- src/condicionales/EjercicioOcho.java
+++ src/condicionales/EjercicioOcho.java
@@ -4,11 +4,15 @@ package condicionales;
 
 public class EjercicioOcho {
 
-    private static int numero = 21;
+    private static int numero = 10;
     private static int primerDigito = numero / 10;
     private static int ultimoDigito = numero % 10;
 
     public static void main(String[] args) {
+        if (numero < 10 || numero > 99) {
+            System.out.println("El número no es de dos digitos");
+            return;
+        }
         if (primerDigito == ultimoDigito) {
             System.out.println("Los digitos son iguales");
         } else {
