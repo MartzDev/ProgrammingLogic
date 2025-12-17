9d130670ba05e594696fc64c547dd1bd2cd8b87c
MartzDev <155332741+MartzDev@users.noreply.github.com>
2025-12-17 18:30:30 -0500

    :hammer: logica del ejercicio diez actualizado

--- src/condicionales/EjercicioDiez.java
+++ src/condicionales/EjercicioDiez.java
@@ -4,22 +4,28 @@ package condicionales;
 
 public class EjercicioDiez {
 
-    private static int primerNumero = 45;
-    private static int segundoNumero = 34;
+    private static int primerNumero = 25;
+    private static int segundoNumero = 32;
     private static int primerDigitoPrimerNumero = primerNumero / 10;
     private static int segundoDigitoPrimerNumero = primerNumero % 10;
     private static int primerDigitoSegundoNumero = segundoNumero / 10;
     private static int segundoDigitoSegundoNumero = segundoNumero % 10;
 
     public static void main(String[] args) {
-        if (primerDigitoPrimerNumero == primerDigitoSegundoNumero
-                || primerDigitoPrimerNumero == segundoDigitoSegundoNumero ||
-                segundoDigitoPrimerNumero == primerDigitoSegundoNumero
-                || segundoDigitoPrimerNumero == segundoDigitoSegundoNumero) {
-            System.out.println("Hay digitos iguales entre los dos numeros");
-        } else {
-            System.out.println("No hay digitos iguales entre los dos numeros");
 
+        if ((primerNumero < 10 || primerNumero > 99) || (segundoNumero < 10 || segundoNumero > 99)) {
+            System.out.println("Ambos numeros deben ser de dos digitos");
+            return;
+        } else {
+            if (primerDigitoPrimerNumero == primerDigitoSegundoNumero
+                    || primerDigitoPrimerNumero == segundoDigitoSegundoNumero ||
+                    segundoDigitoPrimerNumero == primerDigitoSegundoNumero
+                    || segundoDigitoPrimerNumero == segundoDigitoSegundoNumero) {
+                System.out.println("Hay digitos iguales entre los dos numeros");
+            } else {
+                System.out.println("No hay digitos iguales entre los dos numeros");
+
+            }
         }
     }
 }
