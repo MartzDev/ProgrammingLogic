a7c0166636ed6b824a1ad3b255cc3422ca5e01dd
MartzDev <155332741+MartzDev@users.noreply.github.com>
2025-12-17 18:33:14 -0500

    :hammer: logica del ejercicio cinco actualizado

--- src/condicionales/EjercicioCinco.java
+++ src/condicionales/EjercicioCinco.java
@@ -9,11 +9,16 @@ public class EjercicioCinco {
     private static boolean isParesDigitos;
 
     public static void main(String[] args) {
-        digitoUno = numero / 10;
-        digitoDos = numero % 10;
 
-        isParesDigitos = (digitoUno % 2 == 0) && (digitoDos % 2 == 0);
+        if (numero < 10 || numero > 99) {
+            System.out.println("El número no es de dos digitos");
+            return;
+        } else {
+            digitoUno = numero / 10;
+            digitoDos = numero % 10;
+            isParesDigitos = (digitoUno % 2 == 0) && (digitoDos % 2 == 0);
 
-        System.out.println(isParesDigitos ? "Ambos digitos son pares" : "Al menos un digito no es par");
+            System.out.println(isParesDigitos ? "Ambos digitos son pares" : "Al menos un digito no es par");
+        }
     }
 }
